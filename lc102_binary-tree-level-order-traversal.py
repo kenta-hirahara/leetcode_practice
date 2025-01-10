bbd0149df5de43e27ab45b2f3064221ef7639dd2
kenta-hirahara <kenta.hirahara@student.kit.edu>
2025-01-10 23:54:20 +0100

    input type is now list of integers.

--- lc102_binary-tree-level-order-traversal.py
+++ lc102_binary-tree-level-order-traversal.py
@@ -22,12 +22,8 @@ def levelOrder(root: TreeNode) -> list[list[int]]:
 
     return result
 
-# Example usage:
-# Constructing the binary tree [3, 9, 20, None, None, 15, 7]
-root = TreeNode(3)
-root.left = TreeNode(9)
-root.right = TreeNode(20)
-root.right.left = TreeNode(15)
-root.right.right = TreeNode(7)
+data = [5, 3, 2, 9, None, 4, 11, -3, 8, 0, None, 7, 1]
+
+root = list_to_tree_node(data)
 
 print(levelOrder(root))
