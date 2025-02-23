bbb1c6ee7eba6b5cc806a24a1c607a9a6cc6596f
kenta-hirahara <kenta.hirahara@student.kit.edu>
2025-02-23 16:37:10 +0100

    manual print removed

--- hash_map/solutions/lc1_two_sum.py
+++ hash_map/solutions/lc1_two_sum.py
@@ -5,6 +5,3 @@ def twoSum(nums: list[int], target: int) -> list[int]:
             hashmap[target - num] = i
         else:
             return [hashmap[num], i]
-
-print(twoSum([2,7,11,15], 9))
-print(twoSum([3,2,4], 6))
